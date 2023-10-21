1872434a4a003b4ef3d32f855ae5c7ba91740401
geumyoung <1gyn093@gmail.com>
2023-10-21 15:14:48 +0900

    useInput 구조 분해 재할당

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,27 +2,24 @@ import { useState } from 'react';
 import useInput from '../hooks/use-input';
 const SimpleInput = () => {
   // const { name, email, nameInputHandler, emailInputHandler } = useInput();
-  const { enterText, enteredInputHandler } = useInput();
+  const { enteredText: name, enteredInputHandler: nameInputHandler } =
+    useInput();
+  const { enteredText: email, enteredInputHandler: emailInputHandler } =
+    useInput();
 
-  // const [name, setName] = useState('');
   const [nameTouched, setNameTouched] = useState(false); //touched : true
-
-  // const [email, setEmail] = useState('');
   const [emailTouched, setEmailTouched] = useState(false);
 
-  const isNameValid = enterText.name.trim().length > 0;
-  // .trim() !== ''
+  const isNameValid = name.trim().length > 0; // .trim() !== ''
   // 재평가가 너무 많아지지 않게, 그냥 변수로 준다.
   const isNameInValid = !isNameValid && nameTouched;
   // isNameValid(입력값이 없을 때) , nameTouched(false, ) : 입력값이 없고 (false), 입력창을 touch 햇음 (ture)
-  const isEmailValid = enterText.email.includes('@');
+  const isEmailValid = email.includes('@');
   const isEmailInValid = !isEmailValid && emailTouched;
   // isEmailValid의 입력값이 없고(false), 입력창을 touch했음 (true)
 
-  // const nameInputHandler = (e) => setName(e.target.value);
   const nameBlurHandler = (e) => setNameTouched(true);
 
-  // const emailInputHandler = (e) => setEmail(e.target.value);
   const emailBlurHandler = (e) => setEmailTouched(true);
 
   const submitHandler = (e) => {
@@ -48,8 +45,8 @@ const SimpleInput = () => {
         <input
           type="text"
           id="name"
-          value={enterText.name}
-          onChange={enteredInputHandler}
+          value={name}
+          onChange={nameInputHandler}
           onBlur={nameBlurHandler}
         />
         {isNameInValid && <p className="error-text">이름을 입력하세요.</p>}
@@ -60,8 +57,8 @@ const SimpleInput = () => {
         <input
           type="text"
           id="email"
-          value={enterText.email}
-          onChange={enteredInputHandler}
+          value={email}
+          onChange={emailInputHandler}
           onBlur={emailBlurHandler}
         />
         {isEmailInValid && (
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -2,33 +2,15 @@ import { useState } from 'react';
 
 const useInput = () => {
   //공통 state만들기
-  const [enterText, setEnterText] = useState({
-    name: '',
-    email: '',
-  });
-  // 공통 input handler 만들기
+  const [enteredText, setEnteredText] = useState('');
 
+  // 공통 input handler 만들기
   const enteredInputHandler = (e) => {
-    const { id, value } = e.target;
-    // e.target에서 id, value 값을 그대로  추출
-    setEnterText({
-      ...enterText,
-      [id]: value,
-    });
-    console.log('enterText___', enterText);
+    setEnteredText(e.target.value);
+    console.log('enteredText___', enteredText);
   };
 
-  return { enterText, enteredInputHandler };
-
-  //   const [name, setName] = useState('');
-  //   const [email, setEmail] = useState('');
-
-  //   const nameInputHandler = (e) => {
-  //     setName(e.target.value);
-  //   };
-  //   const emailInputHandler = (e) => setEmail(e.target.value);
-
-  //   return { name, email, emailInputHandler, nameInputHandler };
+  return { enteredText, enteredInputHandler };
 };
 
 export default useInput;
