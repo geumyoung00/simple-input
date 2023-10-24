f811ee35b3ded867cb7e16295fa604472f82ba2e
geumyoung <1gyn093@gmail.com>
2023-10-24 21:48:58 +0900

    유효성검사를 위한 state 생성

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,21 +1,39 @@
+import { useState } from 'react';
+
 const BasicForm = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [firstNameInputTouched, setFirstNameInputTouched] = useState(false);
+  const [lastNameInputTouched, setLastNameInputTouched] = useState(false);
+  const [emailInputTouched, setEmailInputTouched] = useState(false);
+
+  const isFirstNameValid = firstName.trim().length > 0;
+  const isFirstNameInputInvalid = !isFirstNameValid && firstNameInputTouched;
+
+  const isLastNameValid = lastName.trim().length > 0;
+  const isLastNameInputInvalid = !isLastNameValid && lastNameInputTouched;
+
+  const isEmailValid = email.includes('@');
+  const isEmailInputInvalid = !isEmailValid && emailInputTouched;
+
   return (
     <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+      <div className="control-group">
+        <div className="form-control">
+          <label htmlFor="name">First Name</label>
+          <input type="text" id="name" />
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
