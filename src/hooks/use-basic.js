cdf9c8a206b50dd29e66870749d8dde1e40b80f8
geumyoung <1gyn093@gmail.com>
2023-10-24 23:27:59 +0900

    useBasicInput으로 공통된 로직 이동

--- src/hooks/use-basic.js
+++ src/hooks/use-basic.js
@@ -1,9 +1,28 @@
 import { useState } from 'react';
 
-const useBasicInput = () => {
-  const [entredText, setEnteredText] = useState;
+const useBasicInput = (validate) => {
+  const [entredText, setEnteredText] = useState('');
+  const [inputTouched, setInputTouched] = useState(false);
 
-  return { entredText };
+  const isEntredTextValid = validate(entredText);
+  const isInputInvalid = !isEntredTextValid && inputTouched;
+
+  const inputHandler = (e) => setEnteredText(e.target.value);
+  const inputBlurHandler = () => setInputTouched(true);
+  const resetinput = () => {
+    setEnteredText('');
+    setInputTouched(false);
+  };
+
+  return {
+    entredText,
+    inputHandler,
+    inputTouched,
+    inputBlurHandler,
+    isEntredTextValid,
+    isInputInvalid,
+    resetinput,
+  };
 };
 
 export default useBasicInput;
